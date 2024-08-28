eae832a7f995e218ae44878f7b64d5359c103f96
kwaksss <kis2690@naver.com>
2024-08-28 10:58:25 +0900

    투표ui추가

--- app.js
+++ app.js
@@ -170,6 +170,7 @@ app.get('/about', (req,res)=>{
     goto.go(req,res,{'centerpage':'about'});
 });
 
+
 app.post('/registerimpl', (req,res)=>{
     let id = req.body.id; //name= let id 
     let pwd = req.body.pwd;
@@ -211,6 +212,9 @@ app.post('/registerimpl', (req,res)=>{
 
     const information = require('./routes/information');
     app.use('/information', information)
+
+    const vote1 = require('./routes/vote1');
+    app.use('/vote1', vote1)
 
 
 
--- db/db_sql.js
+++ db/db_sql.js
@@ -6,6 +6,6 @@ module.exports = {
     cust_update:'UPDATE cust SET pwd=?, name=?, acc=? WHERE id=?',
     cust_delete:'DELETE FROM cust WHERE id = ?',
 
-    idol_select: 'SELECT * FROM idol'
+    idol_select: 'SELECT id,name,agency,hobby,specialty,comment,imgname,date_format(birth_date, "%Y년%m월%d일") as birth_date FROM idol'
 
 }
